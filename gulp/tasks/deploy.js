dd09877544d70fd12d6542d0fd3ffd83e05690d1
jlgfmhldf <jlgfmhldf@gmail.com>
2015-10-21 09:35:37 +0300

    change task deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -6,6 +6,6 @@ gulp.task('deploy', () => (
 	runSequence(
 		'del',
 		'build',
-		() => gulp.src('dist/**/*').pipe(ghpages({branch: 'dist'}))
+		() => gulp.src('dist/**/*').pipe(ghpages({branch: 'gh-pages'}))
 	)
 ));
