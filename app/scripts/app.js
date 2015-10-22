6e195b15f988844b4bbb657527f3f93fa3079fad
jlgfmhldf <jlgfmhldf@gmail.com>
2015-10-22 09:18:52 +0300

    fix ios scroll & add mobile pp

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@ $(() => {
         $menuBtn = $('.js-menu-btn'),
         $menuClose = $('.js-close-menu'),
         $sliderMenu = $('.js-mobile-menu'),
+        $container = $('.js-container'),
         $body = $('body'),
         sliderMenuActive = 'slider__mobile-menu_show',
         windowWith = $(window).width(),
@@ -21,18 +22,25 @@ $(() => {
     function mobileMenu() {
         var wHeight = $(window).height();
 
+        function preventDefault(e) {
+            e.preventDefault();
+        }
+
+
         this.show = function () {
             $sliderMenu.addClass(sliderMenuActive);
             $menuBtn.hide();
-            $body.addClass('blocked')
-                .css('height', wHeight + 'px');
+
+            $body.on('touchmove.body', preventDefault).addClass('no-scroll');
+
         };
 
         this.hide = function () {
             $sliderMenu.removeClass(sliderMenuActive);
             $menuBtn.show();
-            $body.removeClass('blocked')
-                .css('height', '');
+
+            $body.off('touchmove.body').removeClass('no-scroll');
+
         }
     }
 
